aeed52ab07cf5439ea07446f932cfb9161af545c
Günalp Uysal <gunalp@beezsoft.com>
2024-05-29 10:20:34 +0300

    hotfix

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,9 @@
 <template>
-  <div :class="theme">
+  <div>
     <h1>Welcome to {{ tenant }}'s page</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const { theme } = useTenantSettings();
 const tenant = useState('tenant').value;
 </script>
-
-<style scoped>
-.light {
-  background-color: white;
-  color: black;
-}
-
-.dark {
-  background-color: black;
-  color: white;
-}
-</style>
